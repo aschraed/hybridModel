4d8bf909fac3981a1ef5e24aa0c9463bb9653130
Andres Iglesias <aiglesias24.ai@gmail.com>
2023-01-16 22:52:39 -0600

    Added input dialog box

--- fuelGrainModel.m
+++ fuelGrainModel.m
@@ -1,23 +1,31 @@
 answer = questdlg("Do you have a screenshot or a black and white image?",'Selection','SS','BW','SS');
-[file,path] = uigetfile;
-img = imread(fullfile(path,file)); % Reads image and saves as variable
+file = imgetfile;
+img = imread(file); % Reads image and saves as variable
 if answer == "SS"
 img = imbinarize(imadjust(rgb2gray(img)));
 img = imfill(img);
 end
 %% Step 1
+
+prompt = {'Heigth and Width (mm): ','Regression Rate (mm/s):','Step Size (s):','Burn Time (s)'};
+dlgtitle = 'Inputs';
+dims = [1 20];
+definp = {'50', '3', '.5', '10'};
+inputs = inputdlg(prompt,dlgtitle,dims,definp);
+inputs = str2double(inputs);
+
 img = imclearborder(img);
 imshow(img);
 nPixX = size(img,1);
 nPixY = size(img,2);
-W = 50; % mm
-H = 50; % mm
+W = inputs(1); % mm
+H = inputs(1); % mm
 dx = W/nPixX; % mm of the width of each pixel
 dy = H/nPixY; % mm of the height of each pixel
-rDot = 3; % Burn Regression
+rDot = inputs(2); % Burn Regression
 rDotPix = rDot/dx; % Regression in pixels
-stepSize = 1; % How many seconds per step
-burnTime = 5; % sec
+stepSize = inputs(3); % How many seconds per step
+burnTime = inputs(4); % sec
 reg = rDotPix*stepSize; 
 steps = burnTime/stepSize;
 xPlot = 1:1:steps;
