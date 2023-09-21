9b52900b5908d841518a196619975521ec320e95
nevangelista545 <nkme@utexas.edu>
2023-09-20 23:31:01 -0500

    adding calculator for HTPB castings

---
+++ CastingCalcs/CurativeCalculation.m
@@ -0,0 +1,34 @@
+% Curative Calculation for HTPB resin and Modified MDI Isocyanate curative
+% from Rocket Motor Components.
+% Uses Equivalent weight calculations from http://www.thrustgear.com/topics/Curative_calculation.pdf
+% Function of the curative is to link the hydroxyl compound (OH) of the
+% binder with an isocyanate compound (NCO) of the curative.
+
+% EW = Effective Weight listed on the MDSs
+prompt = {'EW of Resin (OH Eq. Wt.): ', 'EW of Curative (NCO Eq. Wt): ', 'Index Ratio (0-1, 1 is full cure)'};
+dlgtitle = 'Curative Calculation: Material Properties';
+dims = [1 60];
+definput = {'1219.5', '185', '1'};
+opts.Resize = 'on';
+inputs = inputdlg(prompt, dlgtitle, dims, definput, opts);
+inputs = str2double(inputs(1:3));
+% str2double converts the cell array from the dialog box into a vector
+
+relativeHardness = inputs(3);
+ewResin = inputs(1);
+ewCurative = inputs(2);
+massResin = 100;
+
+% calc required mass of curative based on EW of OH and NCO
+% percent of curative only dependent on Index Ratio, not mass of Resin
+massCurative = relativeHardness * ewCurative * (massResin/ewResin);
+proportionCurative = massCurative / (massCurative + massResin);
+fprintf('Mass of Curative need: %.3f g\n', massCurative)
+fprintf('Percent Curative: %.3f \n', proportionCurative*100)
+
+% finding percent curative for small changes in index ratio
+dIndRat = 1000;
+indRatSave = linspace(0,1,dIndRat);
+for n = 1:dIndRat
+    
+end
---
+++ CastingCalcs/HTPB_Mass_Calc.m
@@ -0,0 +1,54 @@
+% running this script will produce a dialogue box where you can enter the
+% dimensions of the mold you are using, and lets you enter a
+% user-overridden volume obtained from Solidworks if port geometry is
+% complex
+
+%% composition parameters
+per_MDI = .14; % percentage of resin obtained from CurativeCalculation
+per_R45 = 1 - per_MDI;
+
+% densities of resin and curative from Rocket Motor Components TDS
+rho_R45 = .901;  % g/cm^3
+rho_MDI = 1.22;  % g/cm^3
+
+%% cylindrical grain dimensions
+prompt = {'Solidworks Volume Override (Leave as 0 if inputting dimensions)'...
+    'Effective Grain Length (in)', 'Mold OD (in, outermost wall)'...
+    'Port ID (in)', 'Mold Thickness (cm)', 'Pouring Loss Factor (Scales Mass)'};
+dlgtitle = 'Curative Calculation: Grain Dimensions';
+dims = [1 70];
+definput = {'0', '12', '2', '.5', '.12', '1'}; % Y/N, length, OD, portID, wall thickness, pouring loss factor
+opts.Resize = 'on';
+inputs = inputdlg(prompt, dlgtitle, dims, definput, opts);
+inputs = str2double(inputs(1:6));
+
+if (inputs(1) ~= 0) % checking if user override or not
+    vol_cavity = inputs(1);  % cm^3, user overridden cavity volume
+else
+    eff_grain_length = inputs(2);    % in, total mold length minus the bottom platform
+    mold_OD = inputs(3);    % in, diameter of the outermost wall
+    mold_thickness = inputs(5);   % cm, wall thickness of the print
+    port_ID = inputs(4); % in, inside diameter of the grain port
+    pour_factor = inputs(6); % accounts for pouring losses
+    
+    cavity_OD = (mold_OD*2.54) - (2*mold_thickness);    % cm
+    cavity_ID = (port_ID*2.54) + (2*mold_thickness);    % cm
+    cavity_Area = (pi * ((cavity_OD/2)^2 - (cavity_ID/2)^2)); % cm^2
+    vol_cavity = cavity_Area * (eff_grain_length*2.54);    % cm^3
+end
+
+%% mixture calculations
+% calculates cured mass based on total density and given volume
+rho_total = 1/(per_R45/rho_R45+per_MDI/rho_MDI);  % g/cm^3
+mass_total = rho_total * vol_cavity * pour_factor;  % g
+
+mass_R45 = per_R45 * mass_total;    % g
+mass_PAPI = mass_total - mass_R45;  % g
+xsave = [rho_total mass_total mass_R45 mass_PAPI];
+
+fprintf('Combined Density: %.3f g/cm^3 \n', xsave(1))
+fprintf('Total Mass: %.3f g \n', xsave(2))
+fprintf('Mass Resin: %.3f g \n', xsave(3))
+fprintf('Mass MDI: %.3f g \n', xsave(4))
+% disp("Combined Density (g/cm^3), Total Mass (g), Mass Resin (g), Mass PAPI (g)")
+% disp(xsave)
