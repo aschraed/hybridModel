52efb9a5e5bb56d4393fe6d7330d667329cf5a16
nevangelista545 <124399083+nevangelista545@users.noreply.github.com>
2024-01-27 01:54:50 -0600

    Delete etc/casing.m, extra file generated during merge

--- etc/casing.m
+++
@@ -1,9 +0,0 @@
-clc, clearvars
-ChamberPressure=input('Chamber Pressure');
-Maxstress=input('Max stress');
-SafetyFactor=input('Saftey Factor');
-Radius=input('Radius');
-Thickness=(SafetyFactor*ChamberPressure*Radius)/(Maxstress)
-MEOP=input('Maximum Expected Operating Pressure');
-SafetyFactorBolt=input('Safety factor for bolt shear');
-
